6338fa9e0324a4c71bb15b628ca56bc2fcd45ea6
Skymirrh <skymirrh@gmail.com>
2013-04-03 22:37:04 +0200

    Zip support for dread_parser.py

--- dread_parser.py
+++ dread_parser.py
@@ -11,44 +11,55 @@ import os
 import fileinput
 import tempfile
 import subprocess
+import zlib, zipfile
 
 # Opens the input file (stdin if not given)
 inFile = fileinput.input()
-inFile.readline()
+if zipfile.is_zipfile(inFile):
+    myzip = zipfile.ZipFile(inFile)
+    for filename in myzip.namelist():
+        myFile = myzip.open(filename, 'rU')
+        convert(myFile)
+    myzip.close()
+else:
+    convert(inFile)
 
-# Run showg and store output in a tempFile file
-tempFile = tempfile.NamedTemporaryFile()
-tempFile.readline()
-subprocess.call("\"" +  os.getcwd() + "/\"" + "showg " + "\"" + inFile.filename() + 
-                "\" " + tempFile.name, shell = True)
+def convert(inFile):
+    inFile.readline()
 
-# Looks for the order of the graph in the file
-# Typically, will match "Graph X, order N." and capture N
-for line in tempFile :
-	matchObj = re.match(r'^.+,\s\D+\s(\d+).\s$', line)
-	if matchObj :
-		order = int(matchObj.group(1));
-		break
+    # Run showg and store output in a tempFile file
+    tempFile = tempfile.NamedTemporaryFile()
+    tempFile.readline()
+    subprocess.call("\"" +  os.getcwd() + "/\"" + "showg " + "\"" + inFile.filename() + 
+                    "\" " + tempFile.name, shell = True)
+
+    # Looks for the order of the graph in the file
+    # Typically, will match "Graph X, order N." and capture N
+    for line in tempFile :
+	    matchObj = re.match(r'^.+,\s\D+\s(\d+).\s$', line)
+	    if matchObj :
+		    order = int(matchObj.group(1));
+		    break
 		
-# Stores the edges of the graph in an array
-graphSets = []
-for line in tempFile :
-    node, edges = line.split(':')
-    edges = (edges.replace(';','')).split() # Suppress the final ';' and stores edges
-    graphSets.insert(int(node), edges)
-    
-# Generates the .cub output file
-outputname = inFile.filename().split('.')[0]
-outFile = open(os.getcwd() + "/" + outputname + ".cub", 'w+')
+    # Stores the edges of the graph in an array
+    graphSets = []
+    for line in tempFile :
+        node, edges = line.split(':')
+        edges = (edges.replace(';','')).split() # Suppress the final ';' and stores edges
+        graphSets.insert(int(node), edges)
+        
+    # Generates the .cub output file
+    outputname = inFile.filename().split('.')[0]
+    outFile = open(os.getcwd() + "/" + outputname + ".cub", 'w+')
 
-outFile.write(str(order) + "\t" + "3 \n")
-for i in range(0, order) :
-    outFile.write(str(i) + "\t")
-    edges = "\t".join(graphSets[i])
-    outFile.write(edges + "\n")
-    
-outFile.flush()
+    outFile.write(str(order) + "\t" + "3 \n")
+    for i in range(0, order) :
+        outFile.write(str(i) + "\t")
+        edges = "\t".join(graphSets[i])
+        outFile.write(edges + "\n")
+        
+    outFile.flush()
 
-fileinput.close()
-tempFile.close()
-outFile.close()
+    fileinput.close()
+    tempFile.close()
+    outFile.close()
