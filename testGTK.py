9240563e3e4b60d5c5b8de4ab02bf58adb943870
Gweylorth <g.hudin@gmail.com>
2013-05-01 23:15:42 +0200

    Debut d'implementation de l'operation Theta

--- testGTK.py
+++ testGTK.py
@@ -13,14 +13,14 @@ class PyCubic:
         self.builder = Gtk.Builder()
         self.builder.add_from_file("PyCubic.glade")
         self.graphFrame = self.builder.get_object("graph_frame")
-        self.builder.connect_signals(Handler(self.builder))
+        self.builder.connect_signals(Handler(self.builder, self.graphWidget))
         
     # Add the GraphWidget to display graph g
     def displayGraph(self, g):
         layout = graph_tool.draw.sfdp_layout(g)
-        self.graphDraw = graph_tool.draw.GraphWidget(g, layout)
-        self.graphDraw.show_all()
-        self.graphFrame.add(self.graphDraw)
+        self.graphWidget = graph_tool.draw.GraphWidget(g, layout)
+        self.graphWidget.show_all()
+        self.graphFrame.add(self.graphWidget)
 
 
 class HelpWindow(Gtk.MessageDialog):
@@ -55,15 +55,26 @@ class HelpWindow(Gtk.MessageDialog):
         
 class Handler:
 
-    def __init__(self, builder) :
+    def __init__(self, builder, graphWidget) :
         self.builder = builder
+        self.graphWidget = graphWidget
 
     # Main window delete handler
     def onDeleteWindow(self, *windows):
         Gtk.main_quit(*windows)
+        
+    # Theta button click handler
+    def on_theta_button_clicked(self, button):
+        theta = ThetaHandler(self.graphWidget)
+        self.graphWidget.connect("button_pressed_event", theta.thetabutton_pressed_event)
+        
+    # ThetaMinus button click handler
+    def on_thetaMinus_button_clicked(self, button):
+        theta = ThetaHandler(self.graphWidget)
+        self.graphWidget.connect("button_pressed_event", theta.thetaMinusbutton_pressed_event)
 
     # Help button click handler
-    def on_help_button_clicked(self, helpButton):
+    def on_help_button_clicked(self, button):
         self.helpWindow = HelpWindow()
     
     # Quit Menu Item click handler
@@ -75,6 +86,40 @@ class Handler:
         self.aboutdialog = self.builder.get_object("about_dialog")
         self.aboutdialog.run()
         self.aboutdialog.hide()
+        
+        
+class ThetaHandler:
+
+    def __init__(self, graphWidget):
+        self.graphWidget = graphWidget
+        self.firstPick = None
+        self.secondPick = None
+
+    # GraphWidget click handler while Theta Operation
+    def thetabutton_pressed_event(self, widget, event):
+        if event.button == 1 : # and not event.state & Gdk.ModifierType.CONTROL_MASK :  # TODO Dafuq is la deuxième condition ?
+            if self.graphWidget.picked == False :
+                self.graphWidget.init_picked()
+                self.graphWidget.queue.draw()
+                if self.firstPick == None :
+                    self.firstPick = self.graphWidget.picked
+                elif self.secondPick == None :
+                    self.secondPick = self.graphWidget.picked
+                else :
+                # TODO apply THETA operation, we have our two vertices
+                
+    
+        # Restore to original behaviour
+        self.graphWidget.connect("button_pressed_event", graphWidget.button_pressed_event)
+        
+    
+    # GraphWidget click handler while ThetaMinus Operation
+    def thetaMinusbutton_pressed_event(self, widget, event):
+    
+        # Restore to original behaviour
+        self.graphWidget.connect("button_pressed_event", graphWidget.button_pressed_event)
+    
+
 
 
 with open(sys.argv[1]) as file :
