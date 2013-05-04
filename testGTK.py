427b74670bdfc48d4e1163d64349a1dcc9890568
Gweylorth <g.hudin@gmail.com>
2013-05-04 02:47:05 +0200

    Ajout du changement dynamique de layout, desactivation des boutons lors l'operations

--- testGTK.py
+++ testGTK.py
@@ -16,39 +16,91 @@ class PyCubic:
         
     # Add the GraphWidget to display graph g
     def displayGraph(self, g):
-        layout = graph_tool.draw.sfdp_layout(g)
-        self.graphWidget = GraphWidgetCustom(g, layout)
+        layout = sfdp_layout(g)
+        self.graphWidget = GraphWidgetCustom(self, g, layout)
         self.graphWidget.show_all()
         self.graphFrame.add(self.graphWidget)
         
+    # Set a widget to sensitive
+    def activate_widget(self, widgetName) :
+        self.builder.get_object(widgetName).set_sensitive(True)
+        
+    # Set a widget to insensitive
+    def deactivate_widget(self, widgetName) :
+        self.builder.get_object(widgetName).set_sensitive(False)
+    
+        
 class GraphWidgetCustom(graph_tool.draw.GraphWidget):
 
-    def __init__(self, g, layout) :
-        super(GraphWidgetCustom, self).__init__(g, layout)
+    def __init__(self, instance, g, layout) :
+        super(GraphWidgetCustom, self).__init__(g, layout, update_layout = False)
+        self.instance = instance
         self.theta = False
         self.thetaMinus = False
         self.firstPick = None
         self.secondPick = None
     
+    # Custom Button Press Event, implementing Theta operations
     def button_press_event(self, widget, event):
+        # Desactivate Layout Changing (or the prop map pointers will derpyderp)
+        self.instance.deactivate_widget("layoutMenu")
+        
+        # Theta operation handler
         if self.theta == True :
             print "Theta"
-            self.graphWidget.init_picked()
-            self.graphWidget.queue.draw()
+            self.init_picked()
+            self.queue_draw()
             if self.firstPick == None :
-                self.firstPick = self.graphWidget.picked
+                self.firstPick = self.picked
+                self.picked = None
+                print "First Pick"
             elif self.secondPick == None :
-                self.secondPick = self.graphWidget.picked
-            else :
-                print "Pouet"
-                # TODO apply THETA operation, we have our two vertices
-                self.theta = False
+                self.secondPick = self.picked
+                self.picked = None
+                print "Second Pick"
                 
+                print "Fin Theta"
+                
+                # TODO apply THETA operation, we have our two vertices
+                self.g.clear_vertex(self.firstPick)
+                self.g.clear_vertex(self.secondPick)
+                self.g.remove_vertex(self.firstPick, True)
+                self.g.remove_vertex(self.secondPick, True)
+                self.reset_layout()
+                self.regenerate_surface()
+                self.theta = False
+                self.firstPick = None
+                self.secondPick = None
+                self.reactivate_operations()
+                
+                
+        # Theta Minus operation handler
         elif self.thetaMinus == True :
             print "ThetaMinus"
-            
+            self.thetaMinus = False
+            self.reactivate_operations()
+        
+        # Default behaviour, inherited    
         else : 
-            super(self, widget, event)
+            super(GraphWidgetCustom, self).button_press_event(widget, event)
+            
+    # Reactivate Theta and ThetaMinus buttons
+    def reactivate_operations(self):
+        self.instance.activate_widget("theta_button")
+        self.instance.activate_widget("thetaMinus_button")
+            
+    # Dynamically change the graph layout with various algorithms
+    def change_default_layout(self, algo) :
+        if algo == "random" :
+            self.pos = random_layout(self.g)
+        elif algo == "sfdp" :
+            self.pos = sfdp_layout(self.g)
+        elif algo == "arf" :
+            self.pos = arf_layout(self.g)
+        elif algo == "fruchter" :
+            self.pos = fruchterman_reingold_layout(self.g)
+        self.regenerate_surface()
+        self.fit_to_window()
 
 
 class HelpWindow(Gtk.MessageDialog):
@@ -91,26 +143,49 @@ class Handler:
     def onDeleteWindow(self, *windows):
         Gtk.main_quit(*windows)
         
+    ## BUTTON HANDLERS ##
+        
     # Theta button click handler
     def on_theta_button_clicked(self, button):
         print "Theta button clicked"
-        # TODO Desactivate Theta operations buttons
+        self.builder.get_object("theta_button").set_sensitive(False)
+        self.builder.get_object("thetaMinus_button").set_sensitive(False)
         # TODO Update the Status bar
         self.graphWidget.theta = True
         
     # ThetaMinus button click handler
     def on_thetaMinus_button_clicked(self, button):
         print "Theta Minus button clicked"
-        theta = ThetaHandler(self.graphWidget)
-        self.graphWidget.connect("button_press_event", theta.thetaMinusbutton_pressed_event)
+        self.builder.get_object("theta_button").set_sensitive(False)
+        self.builder.get_object("thetaMinus_button").set_sensitive(False)
+        # TODO Update the Status bar
+        self.graphWidget.thetaMinus = True
 
     # Help button click handler
     def on_help_button_clicked(self, button):
         self.helpWindow = HelpWindow()
+        
+    ## MENU HANDLERS ##
     
     # Quit Menu Item click handler
     def on_quit_menu_activate(self, menuItem):
         Gtk.main_quit(menuItem)
+        
+    # Random Layout Menu click handler
+    def on_random_menu_activate(self, menuItem):
+        self.graphWidget.change_default_layout("random")
+        
+    # SFDP Layout Menu click handler
+    def on_sfdp_menu_activate(self, menuItem):
+        self.graphWidget.change_default_layout("sfdp")
+        
+    # ARF Layout Menu click handler
+    def on_arf_menu_activate(self, menuItem):
+        self.graphWidget.change_default_layout("arf")
+        
+    # Fruchter Layout Menu click handler
+    def on_fruchter_menu_activate(self, menuItem):
+        self.graphWidget.change_default_layout("fruchter")
         
     # About Menu Item click handler
     def on_about_menu_activate(self, menuItem):
