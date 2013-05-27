af05290eb748570658b0263d84ae56b2685cbf9a
Skymirrh <skymirrh@gmail.com>
2013-05-27 21:26:13 +0200

    Release.sh revu

--- release.sh
+++ release.sh
@@ -7,6 +7,8 @@ then
     rm -rf $name
     mkdir $name
     cp -r *.py *.glade *.png COPYING README tests doc $name
+    tar -cvzf $name.tar.gz $name
+    rm -rf $name
 
 else
     echo "Usage: ./release.sh VERSION_NUMBER
